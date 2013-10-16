5c7dcd9bdeb8192bc21a87af0601a59f2f8d8bd1
edaena <edaena@mondello.csres.utexas.edu>
2013-10-15 21:53:03 -0500

    Created BeaconCandidate

--- vision/BeaconDetector.h
+++ vision/BeaconDetector.h
@@ -5,17 +5,17 @@
 #include <vision/BlobDetector.h>
 #include <vision/ObjectDetector.h>
 #include <vision/Classifier.h>
-#include <vision/structures/BallCandidate.h>
+#include <vision/structures/BeaconCandidate.h>
 
 class BeaconDetector : public ObjectDetector {
  public:
   BeaconDetector(DETECTOR_DECLARE_ARGS, Classifier*& classifier, BlobDetector*& blob_detector);
   void detectBeacon(bool topCamera);
   void init(TextLogger* tl){textlogger = tl;};
-  //BeaconCandidate candidates[MAX_BEACON_CANDS];
+  BeaconCandidate candidates[40];
   int candidateCount;
  private:
-  bool findBeacon(WorldObject* beacon, BlobCollection& t, BlobCollection& b, bool topCamera);
+  bool findBeacon(WorldObject* beacon, BlobCollection& t, BlobCollection& b, bool topCamera, int* position);
   TextLogger* textlogger;
   Classifier* classifier_;
   BlobDetector* blob_detector_;
